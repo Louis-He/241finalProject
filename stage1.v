de612007f449bbe57d15cf7c881956e0c195dcbe
Louis-He <1726110778@qq.com>
2018-11-05 11:16:53 -0500

    add rom part

--- stage1.v
+++ stage1.v
@@ -3,6 +3,13 @@ module stage1(CLOCK_50, SW, KEY);
 	input CLOCK_50;
 	input [9:0] SW;
 	input [3:0] KEY;
+
+	contorl c0();
+
+	datapath d0();
+
+
+
 endmodule
 
 module control();
@@ -12,3 +19,4 @@ endmodule
 module datapath();
 
 endmodule
+//  RAM 64 words x 32 bits
