e78842a7b652e72959a47700317b323e40360ccd
Louis-He <1726110778@qq.com>
2018-11-05 10:59:52 -0500

    update

--- stage1.v
+++ stage1.v
@@ -1,9 +1,14 @@
 // stage1
-module stage1
-	(
-		assign always_comb begin
-		assign smommmmmm	
-		end
-	);
+module stage1(CLOCK_50, SW, KEY);
+	input CLOCK_50;
+	input [9:0] SW;
+	input [3:0] KEY;
+endmodule
+
+module control();
+
+endmodule
+
+module datapath();
 
 endmodule
